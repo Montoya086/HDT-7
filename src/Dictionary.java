21a7083106a1690df446a1c6342b55843b0d4e35
Montoya086 <mon21552@uvg.edu.gt>
2022-04-03 15:41:46 -0600

    Arreglo de error al borrar y traductor de archivos

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -11,27 +11,87 @@ public class Dictionary {
 	BinarySearchTree<String, String> FrnToSpan = new BinarySearchTree<String,String>(new StringComparator());
 	BinarySearchTree<String, String> SpanToIng = new BinarySearchTree<String,String>(new StringComparator());
 	BinarySearchTree<String, String> SpanToFrn = new BinarySearchTree<String,String>(new StringComparator());
-	Recorrido<String> recorrido = new Recorrido<>();
+	
 	public void insertWord(String Ing, String Span, String Frn) {
-		IngToSpan.insert(Ing,Span);
-		FrnToSpan.insert(Frn,Span);
-		SpanToIng.insert(Span,Ing);
-		SpanToFrn.insert(Span,Frn);
+		IngToSpan.insert(Ing.toLowerCase(),Span.toLowerCase());
+		FrnToSpan.insert(Frn.toLowerCase(),Span.toLowerCase());
+		SpanToIng.insert(Span.toLowerCase(),Ing.toLowerCase());
+		SpanToFrn.insert(Span.toLowerCase(),Frn.toLowerCase());
 	}
 	public String showIngToSpan() {
+		Recorrido<String> recorrido = new Recorrido<>();
 		IngToSpan.inOrder(recorrido);
 		String str ="";
 		for(int i=0;i<IngToSpan.count();i++) {
-			str+="("+SpanToIng.find(recorrido.miLista.get(i))+","+recorrido.miLista.get(i)+")";
+			//if(SpanToIng.find(recorrido.miLista.get(i))!=null) {
+				str+="("+SpanToIng.find(recorrido.miLista.get(i))+","+recorrido.miLista.get(i)+")";
+			//}
 		}
 		return str;
 	}
 	public String showFrnToSpan() {
+		Recorrido<String> recorrido = new Recorrido<>();
 		FrnToSpan.inOrder(recorrido);
 		String str ="";
 		for(int i=0;i<FrnToSpan.count();i++) {
-			str+="("+SpanToFrn.find(recorrido.miLista.get(i))+","+recorrido.miLista.get(i)+")";
+			//if(SpanToFrn.find(recorrido.miLista.get(i))!=null) {
+				str+="("+SpanToFrn.find(recorrido.miLista.get(i))+","+recorrido.miLista.get(i)+")";
+			//}
 		}
 		return str;
+	}
+	public String deleteWord(String word) {
+		String ingWord="";
+		String spanWord="";
+		String frnWord="";
+		if(IngToSpan.find(word)!=null) {
+			ingWord=word;
+			spanWord=IngToSpan.find(word);
+			frnWord=SpanToFrn.find(spanWord);
+			IngToSpan.delete(ingWord);
+			FrnToSpan.delete(frnWord);
+			SpanToFrn.delete(spanWord);
+			SpanToIng.delete(spanWord);
+		}else if (FrnToSpan.find(word)!=null){
+			frnWord=word;
+			spanWord=FrnToSpan.find(word);
+			ingWord=SpanToIng.find(spanWord);
+			IngToSpan.delete(ingWord);
+			FrnToSpan.delete(frnWord);
+			SpanToFrn.delete(spanWord);
+			SpanToIng.delete(spanWord);
+		}else if(SpanToIng.find(word)!=null) {
+			spanWord=word;
+			frnWord=SpanToFrn.find(word);
+			ingWord=SpanToIng.find(word);
+			IngToSpan.delete(ingWord);
+			FrnToSpan.delete(frnWord);
+			SpanToFrn.delete(spanWord);
+			SpanToIng.delete(spanWord);
+		}else {
+			ingWord=null;
+		}
+		return ingWord;
+	}
+	public String searchWord(String word) {
+		String ingWord="";
+		String spanWord="";
+		String frnWord="";
+		if(IngToSpan.find(word)!=null) {
+			ingWord=word;
+			spanWord=IngToSpan.find(word);
+			frnWord=SpanToFrn.find(spanWord);
+		}else if (FrnToSpan.find(word)!=null){
+			frnWord=word;
+			spanWord=FrnToSpan.find(word);
+			ingWord=SpanToIng.find(spanWord);
+		}else if(SpanToIng.find(word)!=null) {
+			spanWord=word;
+			frnWord=SpanToFrn.find(word);
+			ingWord=SpanToIng.find(word);
+		}else {
+			spanWord=null;
+		}
+		return spanWord;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -41,7 +43,7 @@ public class Main {
 			System.out.println("2. Mostrar Diccionario Frances-Español");
 			System.out.println("3. Agregar palabra al diccionario");
 			System.out.println("4. Borrar palabra del diccionario");
-			System.out.println("5. Leer arhivo");
+			System.out.println("5. Leer y traducir arhivo");
 			System.out.println("6. Salir");
 			opt = Integer.parseInt(scanner.nextLine());
 			switch (opt) {
@@ -63,15 +65,58 @@ public class Main {
 				if(validStrings.length==3) {
 					dictArrayList.add(wordString);
 					dictionary.insertWord(validStrings[0], validStrings[1], validStrings[2]);
+					saveFile(dictArrayList);
 					System.out.println("Palabra agregada");
 				}else {
 					System.out.println("La palabra no esta en formato correcto");
 				}
-				System.out.println("\n---**---");
+				System.out.println("---**---\n");
 				break;
 			case 4: 
+				System.out.println("\n---**---");
+				System.out.println("Ingrese la palaba que desea borrar");
+				String word_delete = scanner.nextLine().toLowerCase();
+				String ing_word_delete=dictionary.deleteWord(word_delete);
+				if(ing_word_delete==null) {
+					System.out.println("La palabra no existe");
+				}else {
+					for(int i=0;i<dictArrayList.size();i++) {
+						String[] tempStrings = dictArrayList.get(i).split(",");
+						if(tempStrings[0].equals(ing_word_delete)) {
+							dictArrayList.remove(i);
+						}
+					}
+					saveFile(dictArrayList);
+					System.out.println("Palabra borrada");	
+				}
+				System.out.println("---**---\n");
 				break;
 			case 5: 
+				try {
+					FileReader r = new FileReader("texto.txt");
+					BufferedReader br = new BufferedReader(r);
+					String line;
+					while((line=br.readLine())!=null){
+						String prntString ="";
+						line=line.replace(".", "");
+						line=line.replace(",", "");
+						line=line.replace(";", "");
+						line=line.replace(":", "");
+						String[] wordStrings = line.split(" ");
+						for(int i =0;i<wordStrings.length;i++) {
+							if(dictionary.searchWord(wordStrings[i])!=null) {
+								wordStrings[i]=dictionary.searchWord(wordStrings[i]);
+							}else {
+								wordStrings[i]="*"+wordStrings[i]+"*";
+							}
+							prntString+=wordStrings[i]+" ";
+						}
+						System.out.println(prntString);
+					}
+					br.close();
+				}catch(IOException e) {
+					System.out.println("Ha ocurrido una excepcion de tipo IO: "+e);
+				}
 				break;
 			case 6: 
 				salir = true;
@@ -81,5 +126,18 @@ public class Main {
 			}
 		}
 	}
+	private static void saveFile(ArrayList<String> words) {
+		try {
+			//Sobreescribe el archivo
+			BufferedWriter bw = new BufferedWriter(new FileWriter("diccionario.txt"));
+			for(int i=0;i<words.size();i++) {
+				bw.write(words.get(i)+"\n");
+			}
+				bw.close();
+		}catch (Exception e) {
+			System.out.println("Ha ocurrido un error de tipo: "+e);
+		}
+	}
+
 
 }
